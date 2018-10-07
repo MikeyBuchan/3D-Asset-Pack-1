0d0353d85ba951c6391904d677c95dae9d137761
MikeyBuchan <psmbgames@hotmail.com>
2018-10-07 18:53:14 +0200

    asd

---
+++ 3D Asset pack Scene/Assets/Script/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+    public float speed;
+    private Vector3 v3;
+
+	void Start () 
+	{
+		
+	}
+	
+
+	void Update () 
+	{
+        v3.x = Input.GetAxis("Horizontal");
+        v3.z = Input.GetAxis("Vertical");
+
+        transform.Translate(v3 * speed * Time.deltaTime);
+	}
+}
